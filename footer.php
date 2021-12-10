89c662dd65cd33b98c7be9fcd0a5c90c263691a2
vladbv <vladimirbhelp@gmail.com>
2021-12-10 13:04:54 +0200

    Changes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <script>
-    
-            
-     jQuery('.inscription-checkbox input[type="checkbox"]').on('click', function () {
+
+     jQuery('.inscription_addbox').on('click', function () {
+          jQuery('.inscription_closebox').attr( "style", "display: block !important;" );
         jQuery('#inscription_textbox_field').attr( "style", "display: block !important;" );
         jQuery('#inscription_textbox').attr( "style", "display: block !important;" );
          jQuery('#inscriptioneik_textbox_field').attr( "style", "display: block !important;" );
@@ -11,4 +11,17 @@
                jQuery('#inscriptioncomment_textbox_field').attr( "style", "display: block !important;" );
              
 });
+          jQuery('.inscription_closebox').on('click', function(){
+              jQuery('.inscription_closebox').attr( "style", "display: none !important;" );
+        jQuery('#inscription_textbox_field').attr( "style", "display: none !important;" );
+        jQuery('#inscription_textbox').attr( "style", "display: none !important;" );
+         jQuery('#inscriptioneik_textbox_field').attr( "style", "display: none !important;" );
+          jQuery('#inscriptionin_textbox_field').attr( "style", "display: none !important;" );
+           jQuery('#inscriptioncity_textbox_field').attr( "style", "display: none !important;" );
+            jQuery('#inscriptionaddress_textbox_field').attr( "style", "display: none !important;" );
+               jQuery('#inscriptioncomment_textbox_field').attr( "style", "display: none !important;" );
+          
+          });
+
+
 </script>
--- functions.php
+++ functions.php
@@ -1,23 +1,27 @@
 <?php
 
-
 add_action( 'woocommerce_after_order_notes', 'wordimpress_custom_checkout_field' );
 
 function wordimpress_custom_checkout_field( $checkout ) {
 
-	// Check if Product is in Cart (UPDATE WITH YOUR PRODUCT ID)
+	//Check if Book in Cart (UPDATE WITH YOUR PRODUCT ID)
 
 
-	// Product is in cart so show additional fields
+	//Book is in cart so show additional fields
 
 		echo '<div id="my_custom_checkout_field"><h3>' . __( 'Ще желаете ли фактура?' ) . '</h3><p style="margin: 0 0 8px;">Желая да ми бъде издадена фактура</p>';
 
-		woocommerce_form_field( 'inscription_checkbox', array(
-			'type'  => 'checkbox',
-			'class' => array( 'inscription-checkbox ' ),
-			'label' => __( 'Да' ),
-		), $checkout->get_value( 'inscription_checkbox' ) );
+		//woocommerce_form_field( 'inscription_checkbox', array(
+		//	'type'  => 'checkbox',
+		//	'class' => array( 'inscription-checkbox ' ),
+		//	'label' => __( 'Да' ),
+		//), $checkout->get_value( 'inscription_checkbox' ) );
+		
+	        echo '<input type="button" id="inscription_addbox" name="inscription_addbox" class="button inscription_addbox" value="Да" />';
 
+		
+		echo '<input type="button" id="inscription_closebox" class="button inscription_closebox" value="Не желая фактура" />';
+		
 		woocommerce_form_field( 'inscription_textbox', array(
 			'type'  => 'text',
 			'class' => array( 'inscription-text ' ),
@@ -75,23 +79,32 @@ function wordimpress_is_conditional_product_in_cart( $product_id ) {
 	//Check to see if user has product in cart
 	global $woocommerce;
 
-	//flag if there is no product in cart
-	$product_in_cart = false;
+	//flag no book in cart
+	$book_in_cart = false;
 
 	foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $values ) {
 		$_product = $values['data'];
 
 		if ( $_product->id === $product_id ) {
-			// product is in cart!
-			$product_in_cart = true;
+			//book is in cart!
+			$book_in_cart = true;
 
 		}
 	}
 
-	return $product_in_cart;
+	return $book_in_cart;
 
 }
 
+
+// Redirect WooCommerce Shop URL
+function wpc_shop_url_redirect() {
+    if( is_shop() ){
+        wp_redirect( home_url( '/продукти/' ) ); // Assign custom internal page here
+        exit();
+    }
+}
+add_action( 'template_redirect', 'wpc_shop_url_redirect' );
 
 
 /**
@@ -102,9 +115,9 @@ add_action( 'woocommerce_checkout_update_order_meta', 'wordimpress_custom_checko
 function wordimpress_custom_checkout_field_update_order_meta( $order_id ) {
 
 	//check if $_POST has our custom fields
-	if ( $_POST['inscription_checkbox'] ) {
+	if ( $_POST['inscription_textbox'] || $_POST['inscriptioneik_textbox'] || $_POST['inscriptionin_textbox'] || $_POST['inscriptioncity_textbox'] || $_POST['inscriptionaddress_textbox'] ) {
 		//It does: update post meta for this order
-		update_post_meta( $order_id, 'Клиентът иска фактура', esc_attr( $_POST['inscription_checkbox'] ) );
+		update_post_meta( $order_id, 'Клиентът иска фактура', esc_attr( 'Да' ) );
 	}
 	if ( $_POST['inscription_textbox'] ) {
 		update_post_meta( $order_id, 'Фирма', esc_attr( $_POST['inscription_textbox'] ) );
@@ -138,10 +151,10 @@ add_filter( 'woocommerce_email_order_meta_keys', 'wordimpress_checkout_field_ord
 
 function wordimpress_checkout_field_order_meta_keys( $keys ) {
 
-	// Check if the product is  in Cart
+	//Check if Book in Cart
 
 
-	// Only if the product is in cart
+	//Only if book in cart
 
 
 		$keys[] = 'Клиентът иска фактура';
